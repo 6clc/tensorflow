0ffab55bf9671108e9ae59a0fd5462263ac616f8
liuchao <chaoliu.lc@qq.com>
2021-10-13 21:19:09 +0800

    sync to tf1.13-lc

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 source liuchao/build.sh env
 bazel build --spawn_strategy=standalone --verbose_failures --local_resources 11048,2.0,2.0 -c dbg --copt -g //tensorflow/tools/pip_package:build_pip_package
-./bazel-bin/tools/pip_package/build_pip_package ./
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package ./
 pip install tensorflow*.whl --force-reinstall --no-deps
+t
